dedb1c7f35e49e6865bc77bbd020628afd18da01
Artem Shafarostov <artyom179@mail.ru>
2018-03-22 23:41:36 +0300

    Added different layers. Fixed bugs.

--- scripts/script.sh
+++ scripts/script.sh
@@ -6,7 +6,6 @@ cd "raw" && mkdir "video" "words"
 for i in `seq $1 $2`
 do
     printf "\n\n------------------------- Downloading $i-th video and words alignment -------------------------\n\n"
-   
     cd "words" && curl "http://spandh.dcs.shef.ac.uk/gridcorpus/s$i/align/s$i.tar" > "s$i.tar" && cd ..
     #cd "video" && curl "http://spandh.dcs.shef.ac.uk/gridcorpus/s$i/video/s$i.mpg_vcd.zip" > "s$i.zip" && cd ..
     #unzip -q "video/s$i.zip" -d "../video" && rm "video/s$i.zip"
